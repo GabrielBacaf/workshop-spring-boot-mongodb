d4148a4c0b07e515e2151992ef2e986f428e792d
GabrielBacaf <gabryelbaca1@gmail.com>
2025-01-07 15:08:35 -0400

    featch: Conectando ao MongoDB com repository e service

--- src/main/java/com/workshop/workshop/resources/UserResource.java
+++ src/main/java/com/workshop/workshop/resources/UserResource.java
@@ -1,6 +1,9 @@
 package com.workshop.workshop.resources;
 
 import com.workshop.workshop.domain.User;
+import com.workshop.workshop.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,11 +17,13 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria", "maria@gmail.com");
-        User alex = new User("2", "Alex Greeen", "alex@gmail.com");
-        return ResponseEntity.ok().body( new ArrayList<>(Arrays.asList(maria, alex)));
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
     }
 
 }
